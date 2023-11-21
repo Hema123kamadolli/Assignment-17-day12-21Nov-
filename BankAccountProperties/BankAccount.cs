6747212c408344dd27b306d51723700819b679a0
Hema123kamadolli <hema.kamadolli@mphasis.com>
2023-11-21 16:21:32 +0530

    Initial commit

---
+++ BankAccountProperties/BankAccount.cs
@@ -0,0 +1,66 @@
+﻿//PART-1
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountProperties
+{
+    public class BankAccount
+    {
+        // Properties
+        public int AccountNumber { get; }
+        public string AccountHolderName { get; set; }
+        private double Balance { get; set; }
+
+        // Static variable to generate unique account numbers
+        private static int nextAccountNumber = 10758001;
+
+        // Constructor
+        public  BankAccount(string accountHolderName)
+        {
+            AccountNumber = nextAccountNumber++;
+            AccountHolderName = accountHolderName;
+            Balance = 0;
+        }
+
+        // Method to deposit money into the account
+        public void Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                Balance += amount;
+                Console.WriteLine($"Deposited {amount}. New balance: {Balance}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid deposit amount.");
+            }
+        }
+
+        // Method to withdraw money from the account
+        public void Withdraw(double amount)
+        {
+            if (amount > 0 && amount <= Balance)
+            {
+                Balance -= amount;
+                Console.WriteLine($"Withdrawn {amount}. New balance: {Balance}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid withdrawal amount or insufficient funds.");
+            }
+        }
+
+        // Method to display account details
+        public void DisplayAccountDetails()
+        {
+            Console.WriteLine($"Account Number: {AccountNumber}");
+            Console.WriteLine($"Account Holder: {AccountHolderName}");
+            Console.WriteLine($"Balance: {Balance}");
+            Console.WriteLine();
+        }
+    }
+}
+
---
+++ BankAccountProperties/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+ 
+
+
+//Part-1
+namespace BankAccountProperties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating an instance of the BankAccount class
+            BankAccount myAccount = new BankAccount("Mohan");
+
+            // Displaying initial account details
+            Console.WriteLine("Initial Account Details:");
+            myAccount.DisplayAccountDetails();
+
+            // Making some transactions
+            myAccount.Deposit(5000);
+            myAccount.Withdraw(500);
+
+            // Displaying updated account details
+            Console.WriteLine("Updated Account Details:");
+            myAccount.DisplayAccountDetails();
+
+            Console.ReadKey();
+        }
+      
+    }
+}
